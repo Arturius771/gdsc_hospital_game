967a2d4149a39ca6fddbf0bdd043cc8f6fce0a84
Arturius771 <fodena@tcd.ie>
2022-03-27 17:37:07 +0100

    added app

--- medi_game_app/lib/main.dart
+++ medi_game_app/lib/main.dart
@@ -2,55 +2,153 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class FirstRoute extends StatelessWidget {
-  const FirstRoute({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+	const MyApp({ Key? key }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Open route'),
-          onPressed: () {
-            // Navigate to second route when tapped.
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SecondRoute()),
-            );
-          },
-        ),
-      ),
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return MaterialApp(
+			title: 'MediGame',
+			home: Scaffold(
+				appBar: AppBar(title: const Text('MediGame')),
+				body: const MediGame(),
+			)
+		);
+	}
 }
 
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
-    );
-  }
+//Welcome Page
+class MediGame extends StatelessWidget {
+	const MediGame({Key? key}) : super(key: key);
+
+	@override
+	Widget build(BuildContext context) {
+		return Center(
+			child: Column(
+				mainAxisSize: MainAxisSize.min,
+				children: <Widget>[
+					const Text(
+						'Welcome to MediGame'
+					),
+					const SizedBox(height: 30),
+					TextButton(
+						style: TextButton.styleFrom(
+						textStyle: const TextStyle(fontSize: 20),
+						),
+						onPressed: () {
+							Navigator.push(
+								context,
+								MaterialPageRoute(builder: (context) => const SignIn())
+							);					
+						},
+						child: const Text('Sign In'),
+					),
+					const SizedBox(height: 30),
+					TextButton(
+						style: TextButton.styleFrom(
+						textStyle: const TextStyle(fontSize: 20),
+						),
+						onPressed: () {
+							Navigator.push(
+								context,
+								MaterialPageRoute(builder: (context) => const SignUp())
+							);
+						},
+						child: const Text('Sign Up'),
+					)
+				]
+			)
+		);
+	}
+}
+
+//Sign In Page
+//Back Button, Username, Password
+class SignIn extends StatelessWidget {
+	const SignIn({Key? key}) : super(key: key);
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			backgroundColor: Colors.white,
+			appBar: AppBar(title: const Text('Enter Your Details')),
+			body: Column(
+				children: <Widget>[
+					TextFormField(
+						decoration: const InputDecoration(
+							border: UnderlineInputBorder(),
+							labelText: 'Username or Email',
+						)
+					),
+					TextFormField(
+						obscureText: true,
+						decoration: const InputDecoration(							
+							border: UnderlineInputBorder(),
+							labelText: 'Password',
+						)
+					),
+					TextButton(
+						style: TextButton.styleFrom(
+						textStyle: const TextStyle(fontSize: 20),
+						),
+						onPressed: () {},
+						child: const Text('Login'),
+					)
+				]
+			)
+		);
+	}
+}
+
+//Sign In Page
+//Back Button, Username, Password
+class SignUp extends StatelessWidget {
+	const SignUp({Key? key}) : super(key: key);
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			backgroundColor: Colors.white,
+			appBar: AppBar(title: const Text('Create an Account')),
+			body: Column(
+				children: <Widget>[
+					TextFormField(
+						decoration: const InputDecoration(
+							border: UnderlineInputBorder(),
+							labelText: 'Username',
+						)
+					),
+					TextFormField(
+						decoration: const InputDecoration(
+							border: UnderlineInputBorder(),
+							labelText: 'Email',
+						)
+					),
+					TextFormField(
+						obscureText: true,
+						decoration: const InputDecoration(							
+							border: UnderlineInputBorder(),
+							labelText: 'Password',
+						)
+					),
+					TextButton(
+						style: TextButton.styleFrom(
+						textStyle: const TextStyle(fontSize: 20),
+						),
+						onPressed: () {},
+						child: const Text('Proceed'),
+					)
+				]
+			)
+		);
+	}
 }
