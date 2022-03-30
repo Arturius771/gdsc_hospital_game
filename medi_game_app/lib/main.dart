c0dfa1b7d99ec5bdb05a42b75acd7b5189f424d8
Arturius771 <fodena@tcd.ie>
2022-03-30 15:08:09 +0100

    fixed laptop change error

--- medi_game_app/lib/main.dart
+++ medi_game_app/lib/main.dart
@@ -1,205 +1,188 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-	const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-			title: 'MediGame',
-			home: Scaffold(
-				appBar: AppBar(title: const Text('MediGame')),
-				body: const MediGame(),
-			)
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'MediGame',
+        home: Scaffold(
+          appBar: AppBar(title: const Text('MediGame')),
+          body: const MediGame(),
+        ));
+  }
 }
-
 
 //Welcome Page
 class MediGame extends StatelessWidget {
-	const MediGame({Key? key}) : super(key: key);
+  const MediGame({Key? key}) : super(key: key);
 
-	@override
-	Widget build(BuildContext context) {
-		return Center(
-			child: Column(
-				mainAxisSize: MainAxisSize.min,
-				children: <Widget>[
-					const Text(
-						'Welcome to MediGame'
-					),
-					const SizedBox(height: 30),
-					TextButton(
-						style: TextButton.styleFrom(
-						textStyle: const TextStyle(fontSize: 20),
-						),
-						onPressed: () {
-							Navigator.push(
-								context,
-								MaterialPageRoute(builder: (context) => const SignIn())
-							);					
-						},
-						child: const Text('Sign In'),
-					),
-					const SizedBox(height: 30),
-					TextButton(
-						style: TextButton.styleFrom(
-						textStyle: const TextStyle(fontSize: 20),
-						),
-						onPressed: () {
-							Navigator.push(
-								context,
-								MaterialPageRoute(builder: (context) => const SignUp())
-							);
-						},
-						child: const Text('Sign Up'),
-					)
-				]
-			)
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      const Text('Welcome to MediGame'),
+      const SizedBox(height: 30),
+      TextButton(
+        style: TextButton.styleFrom(
+          textStyle: const TextStyle(fontSize: 20),
+        ),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const SignIn()));
+        },
+        child: const Text('Sign In'),
+      ),
+      const SizedBox(height: 30),
+      TextButton(
+        style: TextButton.styleFrom(
+          textStyle: const TextStyle(fontSize: 20),
+        ),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const SignUp()));
+        },
+        child: const Text('Sign Up'),
+      )
+    ]));
+  }
 }
 
 //Sign In Page
 //Back Button, Username, Password
 class SignIn extends StatelessWidget {
-	const SignIn({Key? key}) : super(key: key);
+  const SignIn({Key? key}) : super(key: key);
 
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			backgroundColor: Colors.white,
-			appBar: AppBar(title: const Text('Enter Your Details')),
-			body: Column(
-				children: <Widget>[
-					TextFormField(
-						decoration: const InputDecoration(
-							border: UnderlineInputBorder(),
-							labelText: 'Username or Email',
-						)
-					),
-					TextFormField(
-						obscureText: true,
-						decoration: const InputDecoration(							
-							border: UnderlineInputBorder(),
-							labelText: 'Password',
-						)
-					),
-					TextButton(
-						style: TextButton.styleFrom(
-						textStyle: const TextStyle(fontSize: 20),
-						),
-						onPressed: () {
-							Navigator.push(
-								context,
-								MaterialPageRoute(builder: (context) => const Home())
-							);
-						},
-						child: const Text('Login'),
-					)
-				]
-			)
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(title: const Text('Enter Your Details')),
+        body: Column(children: <Widget>[
+          const SizedBox(height: 30),
+          TextFormField(
+              decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            labelText: 'Username or Email',
+          )),
+          const SizedBox(height: 30),
+          TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Password',
+              )),
+          const SizedBox(height: 30),
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 20),
+            ),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Home()));
+            },
+            child: const Text('Login'),
+          )
+        ]));
+  }
 }
 
 //Sign In Page
 //Back Button, Username, Password
 class SignUp extends StatelessWidget {
-	const SignUp({Key? key}) : super(key: key);
+  const SignUp({Key? key}) : super(key: key);
 
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			backgroundColor: Colors.white,
-			appBar: AppBar(title: const Text('Create an Account')),
-			body: Column(
-				children: <Widget>[
-					TextFormField(
-						decoration: const InputDecoration(
-							border: UnderlineInputBorder(),
-							labelText: 'Username',
-						)
-					),
-					TextFormField(
-						decoration: const InputDecoration(
-							border: UnderlineInputBorder(),
-							labelText: 'Email',
-						)
-					),
-					TextFormField(
-						obscureText: true,
-						decoration: const InputDecoration(							
-							border: UnderlineInputBorder(),
-							labelText: 'Password',
-						)
-					),
-					TextButton(
-						style: TextButton.styleFrom(
-						textStyle: const TextStyle(fontSize: 20),
-						),
-						onPressed: () {
-							Navigator.push(
-								context,
-								MaterialPageRoute(builder: (context) => const Home())
-							);
-						},
-						child: const Text('Proceed'),
-					)
-				]
-			)
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(title: const Text('Create an Account')),
+        body: Column(children: <Widget>[
+          TextFormField(
+              decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            labelText: 'Username',
+          )),
+          TextFormField(
+              decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            labelText: 'Email',
+          )),
+          TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Password',
+              )),
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 20),
+            ),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Home()));
+            },
+            child: const Text('Proceed'),
+          )
+        ]));
+  }
 }
 
 //Home Page
 //Search, Settings, Welcome Text, Account
 class Home extends StatelessWidget {
-	const Home({Key? key}) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			backgroundColor: Colors.white,
-			appBar: AppBar(title: const Text('Select an Activity')),
-			body: GridView.count(
-				crossAxisCount: 2,
-				children: [
-					TextButton(
-						style: TextButton.styleFrom(
-						textStyle: const TextStyle(fontSize: 20),
-						),
-						onPressed: () {},
-						child: const Text('X-Ray'),
-					),
-					TextButton(
-						style: TextButton.styleFrom(
-						textStyle: const TextStyle(fontSize: 20),
-						),
-						onPressed: () {},
-						child: const Text('Vaccination'),
-					),
-					TextButton(
-						style: TextButton.styleFrom(
-						textStyle: const TextStyle(fontSize: 20),
-						),
-						onPressed: () {},
-						child: const Text('Check Up'),
-					),
-					TextButton(
-						style: TextButton.styleFrom(
-						textStyle: const TextStyle(fontSize: 20),
-						),
-						onPressed: () {},
-						child: const Text('Bandage'),
-					),
-				],
-			)
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(title: const Text('Select an Activity')),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: [
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              onPressed: () {},
+              child: const Text('X-Ray'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              onPressed: () {},
+              child: const Text('Vaccination'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              onPressed: () {},
+              child: const Text('Check Up'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              onPressed: () {},
+              child: const Text('Bandage'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              onPressed: () {},
+              child: const Text('Settings'),
+            ),
+          ],
+        ));
+  }
 }
